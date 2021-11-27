8bf46f1a0335f5db28a5e757038076975e7a2852
hariaja <aldiamahari16@gmail.com>
2021-11-28 02:02:25 +0700

    Perubahan File Koneksi

--- connection/connect.php
+++ connection/connect.php
@@ -1,8 +1,18 @@
-<?php 
+<?php
 
-$host = "localhost";
+// $host = "localhost";
+// $user = "root";
+// $pass = "root";
+// $database = "mahasiswa_api_db";
+
+// $conn = mysqli_connect($host, $user, $pass, $database) or die("Database mySQL Tidak Terhubung");
+
 $user = "root";
 $pass = "";
-$database = "mahasiswa_api_db";
+$host = "localhost";
+$dbdb = "mahasiswa_api_db";
 
-$conn = mysqli_connect($host, $user, $pass, $database) or die("Database mySQL Tidak Terhubung");
+$conn = new mysqli($host, $user, $pass, $dbdb);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
